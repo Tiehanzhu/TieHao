68e0efde0213cbb38c7bdc81f7a7caca63961072
zhwy1hao <1421603025@qq.com>
2022-06-02 22:39:10 +0800

    123

---
+++ 6.2/7.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d %d", &a, &b);
+//	if (a > b)
+//		printf("%d", a);
+//	else
+//		printf("%d", b);
+//	return 0;
+//}
+int main()
+{
+    printf("Name    Age    Gender\n");
+    printf("---------------------\n");
+    printf("Jack\t18\tman");
+    return 0;
+}
