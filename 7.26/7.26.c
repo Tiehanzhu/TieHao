99d6c0ec53ef9ffede3c10bad0cb318389836243
zhwy1hao <1421603025@qq.com>
2022-07-26 21:40:01 +0800

    7.26

---
+++ 7.26/7.26.c
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a[10] = {1,2,3,4,5,6,7,8,9,0};
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//	return 0;
+//}
+
+//void Maopao(int* arr[])
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 10; i > 0; i--)
+//	{
+//		for (j = 0; j < i - 1; j++)
+//		{
+//			if (arr[j] < arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int a[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	Maopao(a);
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = &a[0];
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", *(p+i)); 
+//	}
+//	return 0;
+//}
+// arr[i] ==> *(arr + i) ==> *(i + arr) ==> i[arr]
+
+//int main()
+//{
+//	int shuzu[10] = { 0 };
+//	printf("%p  %p\n", shuzu,shuzu+1);
+//	printf("%p  %p\n", &shuzu[0],&shuzu[0]+1);
+//	printf("%p  %p\n", &shuzu,&shuzu+1);
+//	return 0;
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
